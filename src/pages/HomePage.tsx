e22b6fe82074897ea594cd531ebe4d89b535572c
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2020-09-01 14:39:33 +0700

    Added homepage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "typeface-roboto";
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        fontFamily: "Roboto"
+      }}
+    >
+    </div>
+  );
+};
+
+export default HomePage;

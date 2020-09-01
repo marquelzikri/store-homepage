6eeb3c7355e8eca9853a154e98b9b1927f676af5
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2020-09-01 14:51:39 +0700

    Added NavHeader component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import "typeface-roboto";
 
+import NavHeader from '../components/NavHeader';
 const HomePage = () => {
   return (
     <div
@@ -9,6 +10,7 @@ const HomePage = () => {
         fontFamily: "Roboto"
       }}
     >
+      <NavHeader />
     </div>
   );
 };

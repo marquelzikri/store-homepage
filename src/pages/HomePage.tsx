b7d300754c87e656cef61937895cbcd88cd84d98
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2020-09-01 15:04:11 +0700

    Added lower sections

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,28 @@ const HomePage = () => {
       }}
     >
       <NavHeader />
+      <section className="flex justify-center w-screen">
+        <div className="w-full max-w-5xl">
+          <section className="gap-6 grid grid-cols-3">
+            <img src="https://picsum.photos/400" alt="Lorem Picsum" />
+            <img src="https://picsum.photos/400" alt="Lorem Picsum" />
+            <img src="https://picsum.photos/400" alt="Lorem Picsum" />
+          </section>
+
+          <hr className="mt-12" />
+
+          <section className="my-12">
+            <h3 className="font-bold text-sm uppercase">Koleksi lengkap fashion dari brand-brand terbaik</h3>
+            <p className="my-4 text-xs font-semibold">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt feugiat vestibulum. Donec vel tempor dolor. Sed varius pellentesque urna sed viverra. Morbi eget dui sed velit condimentum dapibus. Vestibulum non fringilla metus, in faucibus arcu. Phasellus et mi nunc. Phasellus dolor mauris, bibendum et justo non, faucibus eleifend tellus. Vivamus sit amet quam ac dui viverra finibus.
+            </p>
+            <h3 className="font-bold text-sm uppercase">Belanja fashion di Mavelin Indonesia</h3>
+            <p className="my-4 text-xs font-semibold">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt feugiat vestibulum. Donec vel tempor dolor. Sed varius pellentesque urna sed viverra. Morbi eget dui sed velit condimentum dapibus. Vestibulum non fringilla metus, in faucibus arcu. Phasellus et mi nunc. Phasellus dolor mauris, bibendum et justo non, faucibus eleifend tellus. Vivamus sit amet quam ac dui viverra finibus.
+            </p>
+          </section>
+        </div>
+      </section>
     </div>
   );
 };
